f1292c04fc5d5be83755d798b6381100b9a7a2e7
Bao Nguyen <ntbao.me@gmail.com>
2022-05-22 00:00:19 +0700

    Scaffold movies

---
+++ db/migrate/20220521164741_create_movies.rb
@@ -0,0 +1,11 @@
+class CreateMovies < ActiveRecord::Migration[7.0]
+  def change
+    create_table :movies do |t|
+      t.string :name
+      t.text :description
+      t.string :youtube_code, null: false
+
+      t.timestamps
+    end
+  end
+end
