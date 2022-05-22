c3c641d5ad15c1be8de42f2bace42a9d3bcfb260
Bao Nguyen <ntbao.me@gmail.com>
2022-05-22 15:59:10 +0700

    Implement sign in sign up feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root "movies#index"
+  # match '*path' => redirect('/'), via: :get
+
+  devise_for :users,
+             controllers: { sessions: 'users/sessions', registrations: 'users/registrations' },
+             path: '',
+             path_names: { sign_in: 'login', sign_out: 'logout' },
+             only:        %i[sessions registrations]
+
   resources :movies
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_21_164741) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_22_023559) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,19 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_21_164741) do
     t.string "youtube_code", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
