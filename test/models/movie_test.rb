00fcd76e5a42c0efdaab7a8d0b14b414699bc4a6
Bao Nguyen <ntbao.me@gmail.com>
2022-05-23 01:38:36 +0700

    Add annotate gem

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: movies
+#
+#  id           :bigint           not null, primary key
+#  description  :text
+#  name         :string
+#  youtube_code :string           not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  user_id      :bigint
+#
 require "test_helper"
 
 class MovieTest < ActiveSupport::TestCase
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                 :bigint           not null, primary key
+#  current_sign_in_at :datetime
+#  current_sign_in_ip :string
+#  email              :string           default(""), not null
+#  encrypted_password :string           default(""), not null
+#  last_sign_in_at    :datetime
+#  last_sign_in_ip    :string
+#  sign_in_count      :integer          default(0), not null
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
