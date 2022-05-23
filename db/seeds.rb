8987e27f7910901acd6cc0d263f3d5581bed7ea1
Bao Nguyen <ntbao.me@gmail.com>
2022-05-23 19:55:27 +0700

    increate generate sameple data

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,13 +12,16 @@ youtube_code_3 = %w[19BMMS7c1ts wUTVE-PRjd0 19BMMS7c1ts 2osHRxoxMo0 x8jO2fPZS6k]
 youtube_code_4 = %w[o6KC8B70Q6s zSv4E0c3T0c vynKH6pdH6E 9LFxIYYAkos I9FAPIZRu90]
 youtube_code_5 = %w[VZOcpgCLgEo 2XphNPJIhDs 03B_QwfdDFY 1qHD7prlVZQ hYp1dt4qur4]
 youtube_code_6 = %w[eZOa-v1hvpM 3eBtgPJ7mWc OaDpaKPvvDM 8XBF8OX7RcU 55xeAW_GdyM]
-3.times do |index|
+5.times do |index|
   user = User.create!(email: "user_#{index+1}@test.com", password: [*'a'..'z'].sample(6).join)
   user.movies.new(name: Faker::Music.band, description: Faker::Lorem.paragraph_by_chars, youtube_code: eval("youtube_code_#{index+1}[0]")).save(validate: false)
   user.movies.new(name: Faker::Music.band, description: Faker::Lorem.paragraph_by_chars, youtube_code: eval("youtube_code_#{index+1}[1]")).save(validate: false)
+  user.movies.new(name: Faker::Music.band, description: Faker::Lorem.paragraph_by_chars, youtube_code: eval("youtube_code_#{index+1}[2]")).save(validate: false)
+  user.movies.new(name: Faker::Music.band, description: Faker::Lorem.paragraph_by_chars, youtube_code: eval("youtube_code_#{index+1}[3]")).save(validate: false)
+  user.movies.new(name: Faker::Music.band, description: Faker::Lorem.paragraph_by_chars, youtube_code: eval("youtube_code_#{index+1}[4]")).save(validate: false)
 end
 
-20.times do
+50.times do
   Movie.all.sample.user_vote_ups.create!(user: User.all.sample)
   Movie.all.sample.user_vote_downs.create!(user: User.all.sample)
 end
